a0ed75119fb2d78a7fdf72a572fec1d58b56100c
jeannette <a01236226@tec.mx>
2023-10-30 21:45:47 -0600

    alergias view
    primera version

--- coreML-nanostarter/3-UI/Account/AlergiasView.swift
+++ coreML-nanostarter/3-UI/Account/AlergiasView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
-struct AlergiasView: View {
+struct Allergy: Identifiable {
+    var id = UUID()
+    var name: String
+    var isChecked: Bool
+}
+
+struct CheckboxField: View {
+    @Binding var isChecked: Bool
+    let text: String
     
     var body: some View {
-        
-        VStack{
-            Text("Aqui podrás marcar tus alergias.")
+        HStack {
+            Image(systemName: isChecked ? "checkmark.square.fill" : "square")
+                .resizable()
+                .frame(width: 25, height: 25)
+                .onTapGesture {
+                    isChecked.toggle()
+                }
+            Text(text)
         }
-        
     }
+}
+
+struct AlergiasView: View {
+    @State private var allergies: [Allergy] = [
+            Allergy(name: "Nueces", isChecked: false),
+            Allergy(name: "Gluten", isChecked: false),
+            Allergy(name: "Lacteos", isChecked: false),
+            Allergy(name: "Soja", isChecked: false),
+            // Agrega más alergias aquí
+        ]
+
+        var body: some View {
+            VStack (alignment:.leading) {
+                Text("Selecciona tus alergias:")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+ 
+                ForEach(allergies.indices, id: \.self) { index in
+                    CheckboxField(isChecked: $allergies[index].isChecked, text: allergies[index].name)
+                    .font(.title)
+                }
+            }
+            .foregroundColor(.green)
+            
+        }
 }
 
 struct AlergiasView_Previews: PreviewProvider {
