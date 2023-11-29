b150ede414df758c66efb84a71c7a29d4d467f1a
Mariano Barberi Garza <88672243+MarianoBarberi@users.noreply.github.com>
2023-11-29 10:16:07 -0600

    Update Classification.swift

--- coreML-nanostarter/2-Data/Classification.swift
+++ coreML-nanostarter/2-Data/Classification.swift
@@ -7,7 +7,7 @@
 
 struct Classification: Decodable {
     // TODO: replace with the name of your keys from mydata.json and some default values
-    var label: String = "Platano"
+    var label: String = "platano"
     var emoji: String = "🍌"
     var video: String = "Sz76m1HzmpU?si=xSIlCbBVOOtY-XKg"
 }
